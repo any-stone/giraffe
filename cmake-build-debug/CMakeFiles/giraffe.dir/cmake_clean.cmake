792886d05a9f22ae8378e397d7c3bbd7209c28b7
any-stone <kuxa.business@gmail.com>
2021-08-08 19:04:01 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/giraffe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/giraffe.dir/main.cpp.o"
+  "giraffe"
+  "giraffe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/giraffe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
